a4912ef6db859ebc0630eda64cfa3642978c98b8
cingu <cingu@DESKTOP-HFK9FFT>
2018-01-23 20:49:04 -0300

    i have seen crazy code..

--- playgroundgcc/src/main/java/playgroundgcc/main/Main.java
+++ playgroundgcc/src/main/java/playgroundgcc/main/Main.java
@@ -3,23 +3,31 @@ package playgroundgcc.main;
 import playgroundgcc.interfaces.UseInterface;
 import playgroundgcc.streamer.LambdaPoc;
 import playgroundgcc.streamer.StreamsPoc;
+import static java.lang.System.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hi!");
+
+		
+		out.print(10);
 		
 		//test lamba poc
 		LambdaPoc lam = new LambdaPoc();
 		
 		//lam.doit();
-		
 		StreamsPoc str = new StreamsPoc();
 		
 		str.doIt();
 		
-		
 		UseInterface use = new UseInterface();
+		
+		double obj = 3.0;
+		Number num = (Number)obj;
+		System.out.println(num);
 	}
+	{System.out.println("kajaja");}
+	
+	static {System.out.println("static");}
 	
 }
